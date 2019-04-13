f7f42e8f6f789f921f3e6581cd05756354a216ee
ahchung23 <hahneol@gmail.com>
2019-04-13 02:44:31 -0400

    changed PORT variable to port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
